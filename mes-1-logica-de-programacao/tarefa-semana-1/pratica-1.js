0e091367a5e12900b8c303a25f03b946197e5621
Mauro Carlos <mauro_carlos14@hotmail.com>
2022-10-05 09:28:57 -0400

    declarar variaveis no pratica-1

--- mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
@@ -3,6 +3,26 @@ let email
 let profissao
 let idade
 let pais
+nome = "mauro Carlos"
+email = "mauro_carlos14@hotmail.com"
+profissao = "tecnico em telecomunicações"
+idade = 55
+pais = "India"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     
