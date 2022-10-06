0edb9fe86dc216c42d73d0244fa4b0115b254b68
Mauro Carlos <mauro_carlos14@hotmail.com>
2022-10-06 13:35:35 -0400

    executar pratica do exercicio pratica-3.js

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-3.js
@@ -0,0 +1,11 @@
+let nome = "mauro Carlos"
+let email = "mauro_carlos14@hotmail.com"
+let profissao = "Técnico em Telecomunicações"
+let idade = 55
+let pais = "India"
+
+console.log (nome == "mauro Carlos")
+console.log (email == "mauro_carlos14@hotmail.com")
+console.log (profissao == "Técnico em Telecomunicações")
+console.log (idade == 55)
+console.log (pais == "India")
