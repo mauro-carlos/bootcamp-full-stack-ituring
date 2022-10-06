19b2d840f0d9f387a38645f9e6e0f01d6af335f2
Mauro Carlos <mauro_carlos14@hotmail.com>
2022-10-06 14:08:14 -0400

    exercicio pratica-5 do bootcamp

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-5.js
@@ -0,0 +1,12 @@
+let nome = "Mauro Carlos"
+let email = "mauro_carlos14@hotmail.com"
+let profissao = "Técnico em Telecomunicações"
+let idade = 55
+let  pais = "Africa do Sul"
+
+
+
+
+
+
+console.log (`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${pais}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
