e7cea9fe850ce905efc54850fc53a27c1e87a4c2
Mauro Carlos <mauro_carlos14@hotmail.com>
2022-10-17 08:48:20 -0400

    pratica de operador ternario|

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-13.js
@@ -0,0 +1,6 @@
+function menorNumero (numero1, numero2) {
+ const menorDosDois = numero1 < numero2 ? numero1 : numero2;
+ return menorDosDois;
+}
+console.log (menorNumero (6,1));
+console.log (menorNumero (0,10));
