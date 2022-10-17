1b8eac0d8afed552386130b225ef199a2cc7827d
Mauro Carlos <mauro_carlos14@hotmail.com>
2022-10-17 10:11:20 -0400

    terminando o exercicio da pratica-7.js

--- mes-1-logica-de-programacao/tarefa-semana-2/pratica-7.js
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-7.js
@@ -1,4 +1,17 @@
-let saudacaoMundo = (pais1, pais2, pais3) => {
+let saudacaoMundo = (lingua1, lingua2, lingua3) => {
+    if (lingua1 === "en"|| lingua2 === "en" || lingua3 === "en") {
+        console.log ("saudação em Ingles : Hello Word")
+    }else if (lingua1 === "pt"|| lingua2 === "pt" || lingua3 === "pt") {
+        console.log("saudação em Portugues Olá Mundo")
+    } else     if (lingua1 === "es"|| lingua2 === "es" || lingua3 === "es") {
+        console.log( "saudação em Espanhol Hola mundo")
+    } else if (lingua1 === ""|| lingua2 === "" || lingua3 === "") {
+        console.log( "saudação Padrão Ola mundo")
+}  
 
-    
 }
+
+saudacaoMundo("");
+saudacaoMundo("es");
+saudacaoMundo("pt");
+saudacaoMundo("en");
