7d56c0ae6e7fb00313dcae6021ee480b469e6ae6
Mauro Carlos <mauro_carlos14@hotmail.com>
2022-10-19 17:11:55 -0400

    executando exercicio pratica-2 de loop

--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-1.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-1.js
@@ -2,10 +2,11 @@ const tabuada = (numero) => {
     let multiplicacao;
     i=1;    
     while (i <= 10) {
-        multiplicacao = numero * i;
+        multiplicacao = numero * i;        
         console.log (`${numero} x ${i} = ${multiplicacao}`);
         i++
 
     }
 }
-tabuada(10);
+
+tabuada(120);
---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-2.js
@@ -0,0 +1,10 @@
+const numerosImpares = function (n) {
+    let i = 1;
+    while (i <= n) {        
+       if (numero = i % 2 == 1) {
+          console.log (i);           
+}
+i++;
+}
+}
+numerosImpares(35);
