f08372a09c5f91cafe8c83f54c5d8cc558c5099c
Mauro Carlos <mauro_carlos14@hotmail.com>
2022-10-20 13:29:33 -0400

    aperfeiçoando o exercicio de arrays pratica-5.js

--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-5.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-5.js
@@ -4,22 +4,25 @@ let listadeFilmes = function (filme) {
     }
 
 function pesquisarFilmes (buscar) {
+
     let filme = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"];      
     let i = 0; 
     let diferente = "underfined"   
+    
     while (i < filme.length) {       
         if (filme[i] == buscar) {
             diferente = "vazio"
             console.log (i);            
-    } 
-        
+    }     
               
     
         i++;
         
 }
-    if (diferente != "vazio") {
+    
+      if (diferente == "underfined") {
          console.log ("underfined")
     }
+
 }
- pesquisarFilmes("friends");
+ pesquisarFilmes("loucos");
